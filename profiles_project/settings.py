a0bda35a9756908f9e49bab5216eb8c8646266eb
Dettlaff <maciekwiatr17@gmail.com>
2020-07-14 15:05:57 +0200

    Update static root

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/static/"
+STATIC_ROOT = "static/"
 
 
 AUTH_USER_MODEL = "profiles_api.UserProfile"
